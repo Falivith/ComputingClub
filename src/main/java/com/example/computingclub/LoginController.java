0edcd60d46fc34e0a0aa0740c7c27d27e7283c63
Falivith <falivith@gmail.com>
2022-06-19 20:13:07 -0300

    Entrar com Admin

--- src/main/java/com/example/computingclub/LoginController.java
+++ src/main/java/com/example/computingclub/LoginController.java
@@ -2,8 +2,16 @@ package com.example.computingclub;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginController {
     @FXML
@@ -16,8 +24,17 @@ public class LoginController {
     private Label welcomeText;
 
     @FXML
-    void btnLoginAction(ActionEvent event) {
-        if(loginField.getText().equals("admin") && passwordField.getText().equals("admin"));
+    void btnLoginAction(ActionEvent event) throws IOException {
+        if (loginField.getText().equals("admin") && passwordField.getText().equals("admin")) {
 
+            //Switch to Profile Builder window
+
+            Parent adminEnter = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("profileBuilderScene.fxml")));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(adminEnter);
+            stage.setTitle("Criação de Perfis (Admin)");
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
