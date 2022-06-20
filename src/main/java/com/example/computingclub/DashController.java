e3dc2bbceb53df1fd3e477a3a338a80f84394d20
NaomiChann <naomi.cmyk@gmail.com>
2022-06-19 21:16:52 -0300

    update das scenes dash e prof, nomeada dos fxid e action

--- src/main/java/com/example/computingclub/DashController.java
+++ src/main/java/com/example/computingclub/DashController.java
@@ -1,4 +1,17 @@
 package com.example.computingclub;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class DashController {
+
+    @FXML
+    private Text dashText;
+
+    @FXML
+    void gotoProfile(ActionEvent event) {
+
+    }
+
 }
